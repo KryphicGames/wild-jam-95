66ced023f9d2e374e78c21abeb3a5b2157f2db13
Intensed <intensed01@gmail.com>
2026-07-14 11:59:03 +0200

    Hide Fade Rect in Editor

--- scripts/scenes/main_menu.gd
+++ scripts/scenes/main_menu.gd
@@ -2,8 +2,10 @@ extends Control
 
 @onready var MusicPlayer = $Music
 @onready var Fade = $Fade/AnimationPlayer
+@onready var FadeRect = $Fade
 
 func _ready():
+	FadeRect.show()
 	Fade.play("fade-in")
 
 func _on_music_finished() -> void:
